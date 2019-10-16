150d8a51593acd62f3045497087531a2ce310391
Deborah Soung <debs@sifive.com>
2019-10-16 16:18:05 -0700

    monitoring status priv

--- src/main/scala/util/CoreMonitor.scala
+++ src/main/scala/util/CoreMonitor.scala
@@ -20,6 +20,7 @@ class CoreMonitorBundle(val xLen: Int) extends Bundle with Clocked {
   val rd1src = UInt(width = 5.W)
   val rd1val = UInt(width = xLen.W)
   val inst = UInt(width = 32.W)
+  val prv = UInt(width = 2.W)
 }
 
 // mark a module that has cores with CoreMonitorBundles
